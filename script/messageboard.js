43b799de26c13e6c4b45829647d2aabb36b27271
DWX256OnGitHub <68551259+DWX256OnGitHub@users.noreply.github.com>
2021-12-05 16:59:59 +0800

    Add files via upload

--- script/messageboard.js
+++ script/messageboard.js
@@ -2,19 +2,28 @@
 function savaStorage(id) {
 	var data = document.getElementById(id).value;
 	var time = new Date().getTime();
-	localStorage.setItem(time, data);
-	alert("Message saved！");
-	loadStorage('msg');
+	if (document.getElementById(id).value.length == 0) {
+		alert("Come on, say something! ");
+	} else {
+		localStorage.setItem(time, data);
+		alert("Message saved！");
+		loadStorage('msg');
+	}
+	var httpRequest = new XMLHttpRequest();//create object
+	httpRequest.open('POST', '/', true); //open connetion
+	httpRequest.setRequestHeader("Content-type","application/x-www-form-urlencoded");//set post header
+	httpRequest.send(data);//post
 }
 //load the message by cycles
 function loadStorage(id) {
-	var result = '<table border="1"><tr><td>' + 'Line' + '</td><td>' + 'Content' + '</td><td>' + 'Time:' + '</td></tr>';//set table format
+	var result = '<table border="1"><tr><td>' + 'Line' + '</td><td>' + 'Content' + '</td><td>' + 'Time:' +
+	'</td></tr>'; //set table format
 	for (var i = 1; i < localStorage.length + 1; i++) {
 		var key = localStorage.key(i);
 		var value = localStorage.getItem(key);
 		var date = new Date();
 		date.setTime(key);
-		result += '<tr><td>' + 'Number ' + i  + '</td><td>' + value + '</td><td>' + date + '</td></tr>'//output table
+		result += '<tr><td>' + 'Number ' + i + '</td><td>' + value + '</td><td>' + date + '</td></tr>' //output table
 	}
 	result += '</table>';
 	var target = document.getElementById(id);
--- script/tools.js
+++ script/tools.js
@@ -1,8 +1,9 @@
-function changeTools1(){
-	var ifr=document.getElementById('tools');//get iframe id
-	ifr.src="https://tool.lu/en_US/";//change its src
+function changeTools1() {
+	var ifr = document.getElementById('tools'); //get iframe id
+	ifr.src = "https://tool.lu/en_US/"; //change its src
 }
-function changeTools2(){
-	var ifr=document.getElementById('tools');//get iframe id
-	ifr.src="https://thuscn.com/lab/";//change its src
+
+function changeTools2() {
+	var ifra = document.getElementById('tools'); //get iframe id
+	ifra.src = "https://thuscn.com/lab/"; //change its src
 }
